8e86e768ed6ad4eede37337562300bf5a1203561
jamf90 <70180507+jamf90@users.noreply.github.com>
2020-11-02 11:01:04 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 date_default_timezone_set('America/Bogota');
 
 require('../vendor/autoload.php');
@@ -60,6 +61,43 @@ $app->post('/guardarlectura', function (Request $request) use ($app) {
    	return $respuesta;
 });
 
+$app->get('/consultardato', function () use ($app) {
+
+$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
+
+$query = "SELECT * FROM motor_view ORDER BY id DESC LIMIT 15";
+
+	$consulta = pg_query($dbconn, $query);
+
+//echo"<br><br>";
+	
+//print_r(pg_fetch_array($consulta,3,PGSQL_NUM));
+
+//echo"<br><br>";
+	
+//$cons_array = pg_fetch_array($consulta,5,PGSQL_ASSOC);
+//print_r($cons_array);
+//echo $cons_array[fecha];
+	
+//echo"<br><br>";
+//$cons_object = pg_fetch_object($consulta);
+//print_r($cons_object);	
+//echo $cons_object -> fecha;
+	
+	$resultArray = array();
+  	while ($row = pg_fetch_array($consulta, null, PGSQL_ASSOC)) {
+    	$resultArray[] = $row;
+  	}
+	
+$jsonResult = json_encode($resultArray, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
+
+  $response = new Response();
+  $response->setContent($jsonResult);
+  $response->setCharset('UTF-8');
+  $response->headers->set('Content-Type', 'application/json');
+
+  return $response;
+});	
 
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
