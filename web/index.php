a2f81740ee3a956e99967d8c5524eccdbff90b76
jamf90 <70180507+jamf90@users.noreply.github.com>
2020-11-02 11:04:25 -0500

    JSON consulta

--- web/index.php
+++ web/index.php
@@ -62,10 +62,12 @@ $app->post('/guardarlectura', function (Request $request) use ($app) {
 });
 
 $app->get('/consultardato', function () use ($app) {
+	
+$dbconn = pg_pconnect("host=ec2-54-152-40-168.compute-1.amazonaws.com port=5432 dbname=da5l2p8fhao45b user=rvjdadbcfsozcx password=d568c86e4a84d477292656b6718984c408f607f5459bca9b6eaf550604dfcf66");
 
-$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
 
-$query = "SELECT * FROM motor_view ORDER BY id DESC LIMIT 15";
+
+$query = "SELECT * FROM lecturas ORDER BY id DESC LIMIT 15";
 
 	$consulta = pg_query($dbconn, $query);
 
